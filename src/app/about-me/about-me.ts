2d087448ec24836c640d97200268ea3ed2eada96
Kessen Quintanilha <kessen.17@gmail.com>
2025-09-26 12:36:16 +0100

    alterações

--- src/app/about-me/about-me.ts
+++ src/app/about-me/about-me.ts
@@ -27,8 +27,21 @@ export class AboutMe {
   linkedinLink = 'https://www.linkedin.com/in/kessenquintanilha/';
   resumeLink = 'assets/files/Curriculo-kessen-it.pdf';
 
-  titulo1= "Sobre Mim";
-  titulo2 = `Desenvolvedora de Sistemas e Software Engineer Junior, com experiência em Gestão de
+
+  titulo1="About me";
+  titulo2= ` Systems Developer and Junior Software Engineer, with experience in Human Resources
+Management. Working as a PHC Technician and on the development of virtual assistants
+and Artificial Intelligence solutions (watsonx), aiming to deliver high-quality projects and
+create a positive impact within the company.
+
+I am committed to quality, innovation, and continuous learning, working to deliver impactful
+projects that bring meaningful value.
+
+I am motivated by continuous learning, and I work to master new technologies and
+develop practical solutions that create a positive impact for the company and its users. `;
+
+titulo3= "Sobre Mim";
+titulo4 = `Desenvolvedora de Sistemas e Software Engineer Junior, com experiência em Gestão de
 Recursos Humanos. Atuando como Técnica PHC e no desenvolvimento de assistentes
 virtuais e soluções de Inteligência Artificial (watsonx), buscando entregar projetos de
 qualidade e gerar impacto positivo na empresa.
@@ -39,14 +52,5 @@ projetos de alto impacto e valor agregado.
 Sou motivada pelo aprendizado contínuo, buscando dominar novas tecnologias e
 desenvolver soluções úteis que gerem impacto positivo para a empresa e seus usuários.`;
 
-  titulo3="About me";
-  titulo4= ` Systems Developer and Junior Software Engineer, with experience in Human Resources
-Management. Working as a PHC Technician and on the development of virtual assistants
-and Artificial Intelligence solutions (watsonx), aiming to deliver high-quality projects and
-create a positive impact within the company.
-I am committed to quality, innovation, and continuous learning, working to deliver impactful
-projects that bring meaningful value.
-I am motivated by continuous learning, and I work to master new technologies and
-develop practical solutions that create a positive impact for the company and its users. `;
 
 }
